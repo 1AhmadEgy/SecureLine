f731f0b6d82842d4c14463dc68fe9b3dc2defa81
1AhmadEgy <114110463+1AhmadEgy@users.noreply.github.com>
2026-09-26 01:45:29 +0200

    db: align Android schema version with migrations

--- app/src/main/java/com/secureline/secureline/database/DatabaseManager.java
+++ app/src/main/java/com/secureline/secureline/database/DatabaseManager.java
@@ -10,7 +10,7 @@ import net.zetetic.database.sqlcipher.SQLiteOpenHelper;
 public class DatabaseManager extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "secureline.db";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 4;
 
     static {
         System.loadLibrary("sqlcipher");

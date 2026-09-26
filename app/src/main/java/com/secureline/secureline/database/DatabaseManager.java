395343b8ca171bdb88fcbf8f2bb3a97815f0411c
1AhmadEgy <114110463+1AhmadEgy@users.noreply.github.com>
2026-09-26 03:46:35 +0200

    Bump encrypted database schema to version 7

--- app/src/main/java/com/secureline/secureline/database/DatabaseManager.java
+++ app/src/main/java/com/secureline/secureline/database/DatabaseManager.java
@@ -10,7 +10,7 @@ import net.zetetic.database.sqlcipher.SQLiteOpenHelper;
 public class DatabaseManager extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "secureline.db";
-    private static final int DB_VERSION = 6;
+    private static final int DB_VERSION = 7;
 
     static {
         System.loadLibrary("sqlcipher");
